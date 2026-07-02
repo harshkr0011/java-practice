76580771027acad4326db8ef2ac21b8b3ac74116
Harsh kumar <149315734+harshkr0011@users.noreply.github.com>
2026-07-02 10:37:25 +0530

    Time: 0 ms (100%), Space: 42.2 MB (17.34%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,32 +1,15 @@
-import java.math.BigInteger;
-
 class Solution {
     public int climbStairs(int n) {
-        return fibonacci(new BigInteger(String.valueOf(n + 1))).intValue();
+    if (n <= 2) return n;
+
+    int prev1 = 2, prev2 = 1;
+
+    for (int i = 3; i <= n; i++) {
+        int curr = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = curr;
     }
-    
-    public static BigInteger fibonacci(BigInteger num) {
-        if (num.compareTo(BigInteger.ZERO) < 0) {
-            throw new IllegalArgumentException("Input must be non-negative");
-        }
 
-        if (num.equals(BigInteger.ZERO)) {
-            return BigInteger.ZERO;
-        }
-
-        if (num.equals(BigInteger.ONE)) {
-            return BigInteger.ONE;
-        }
-
-        BigInteger a = BigInteger.ZERO;  // F(0)
-        BigInteger b = BigInteger.ONE;   // F(1)
-
-        for (BigInteger i = BigInteger.TWO; i.compareTo(num) <= 0; i = i.add(BigInteger.ONE)) {
-            BigInteger next = a.add(b);
-            a = b;
-            b = next;
-        }
-
-        return b;
+    return prev1;
     }
 }
